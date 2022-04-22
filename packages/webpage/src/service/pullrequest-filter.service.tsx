5d6edc28d02c39d5255ce5b9300b22f71052db33
ginxo <emingora@redhat.com>
2022-04-22 08:47:02 +0200

    console.log removed

--- packages/webpage/src/service/pullrequest-filter.service.tsx
+++ packages/webpage/src/service/pullrequest-filter.service.tsx
@@ -24,7 +24,6 @@ const handle = (
   state: PullRequestFilterState = initialState,
   action: any
 ): PullRequestFilterState => {
-  console.log(action.type, action.payload)
   switch (action.type) {
     case ACTION_TYPES.FILTER:
       return {
