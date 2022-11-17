ffc337f21933b8f8120a4eb9ebdde0e864448957
Andrea Lamparelli <a.lamparelli95@gmail.com>
2022-11-17 10:02:14 +0100

    Added some unit tests

--- packages/action/test/lib/github/main.test.js
+++ packages/action/test/lib/github/main.test.js
@@ -9,6 +9,7 @@ jest.mock("fs", () => ({
   })
 }));
 jest.mock("../../../src/utils/bin-utils");
+const { ClientError } = require("../../../src/lib/common");
 const { main } = require("../../../src/lib/github/main");
 const { createOctokitInstance } = require("../../../src/utils/bin-utils");
 const { octokit } = require("../../support/mock-octokit");
@@ -28,13 +29,24 @@ afterEach(() => {
   jest.clearAllMocks();
 });
 
-const defaultArgs = {
+const defaultDefFileArgs = {
   title: "Chain Status",
   subtitle: "Chain Status pull requests statuses",
   token: "github-token",
   definitionFile: __dirname + "/../../support/definition-file.yaml",
   projectFilter: ["droolsjbpm-build-bootstrap"],
-  baseBranchFilter: ["main"]
+  baseBranchFilter: ["main"],
+  branches: ["main", "8.29.x"]
+};
+
+const defaultProjectsArgs = {
+  title: "Chain Status",
+  subtitle: "Chain Status pull requests statuses",
+  token: "github-token",
+  projects: ["kiegroup/droolsjbpm-build-bootstrap", "kiegroup/drools"],
+  projectFilter: ["droolsjbpm-build-bootstrap"],
+  baseBranchFilter: ["main"],
+  branches: ["main", "8.29.x"]
 };
 
 const defaultMetadata = {
@@ -45,9 +57,80 @@ const defaultMetadata = {
 };
 
 describe("github main", () => {
-  test("running github main properly", async () => {
-    await main(defaultArgs, "/tmp/chain-status/", defaultMetadata, 1000, true);
+  test("running with definition file", async () => {
+    await main(
+      defaultDefFileArgs,
+      "/tmp/chain-status/",
+      defaultMetadata,
+      1000,
+      true
+    );
 
     // test writeFileSync
+  });
+
+  test("running with projects list", async () => {
+    await main(
+      defaultProjectsArgs,
+      "/tmp/chain-status/",
+      defaultMetadata,
+      1000,
+      true
+    );
+
+    // test writeFileSync
+  });
+
+  test("without definition file and projects list", async () => {
+    expect(
+      async () =>
+        await main(
+          { ...defaultDefFileArgs, definitionFile: undefined },
+          "/tmp/chain-status/",
+          defaultMetadata,
+          1000,
+          true
+        )
+    ).rejects.toThrow(
+      new ClientError(
+        "You must provide either `definition-file` or `projects` argument"
+      )
+    );
+  });
+
+  test("without empty projects after filtering", async () => {
+    expect(
+      async () =>
+        await main(
+          { ...defaultProjectsArgs, projectFilter: ["not-present-project"] },
+          "/tmp/chain-status/",
+          defaultMetadata,
+          1000,
+          true
+        )
+    ).rejects.toThrow(
+      new ClientError("No projects left after applying the filter")
+    );
+  });
+
+  test("with empty definition file", async () => {
+    expect(
+      async () =>
+        await main(
+          {
+            ...defaultDefFileArgs,
+            definitionFile:
+              __dirname + "/../../support/empty-definition-file.yaml"
+          },
+          "/tmp/chain-status/",
+          defaultMetadata,
+          1000,
+          true
+        )
+    ).rejects.toThrow(
+      new ClientError(
+        "No projects defined in either definition file or projects property"
+      )
+    );
   });
 });
